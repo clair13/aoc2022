e37e00a83d677c11ec252420b35742b485a83452
clair13 <theclairvoyant1313@yahoo.gr>
2022-12-16 15:00:51 +0200

    Solution for day4a

---
+++ day4a/day_4a.rb
@@ -0,0 +1,14 @@
+require 'set'
+
+data = File.readlines('input.txt')
+
+result = data
+  .map(&:chomp)
+  .map { _1.split(/[,-]/) }
+  .map { _1.map(&:to_i) }
+  .map { _1.each_slice(2).to_a }
+  .map { _1.map { |a, b| (a..b).to_set } }
+  .reduce(0) {|count, (a, b)| a.subset?(b) || b.subset?(a) ? count + 1 : count }
+
+
+p result
